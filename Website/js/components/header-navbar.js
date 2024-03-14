5b3642ab9a3e3c26da5f13e0eb00bfc35bfbbd67
OndrejDudacek <ondrej.dudacek@gmail.com>
2024-03-14 14:04:47 +0100

    Remove Figma design link from Design/figmaDesign.md and prepared files, strated working on web elemnt - header-navbar

---
+++ Website/js/components/header-navbar.js
@@ -0,0 +1,25 @@
+export default class Navbar extends HTMLElement {
+    connectedCallback() {
+        this.innerHTML = `
+            <nav>
+                <menu>
+                    <li>
+                        <a href="index.html"><img src="./imgs/logo.svg" alt="logo"></a>
+                    </li>
+                    <li>
+                        <a href="photography.html">Fotografie</a>
+                    </li>
+                    <li>
+                        <a href="services.html"><s>Služby</s></a>
+                    </li>
+                    <li>
+                        <a href="contact.html">Kontakt</a>
+                    </li>
+                    <li>
+                        <a href="about.html">O mně</a>
+                    </li>
+                </menu>
+            </nav>
+        `;
+    }
+}
---
+++ Website/js/index.js
@@ -0,0 +1,3 @@
+import Navbar from "./components/header-navbar.js";
+
+customElements.define("header-navbar", Navbar);
